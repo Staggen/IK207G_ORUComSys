97dc50c09c0bafa16a94845f18114f65958b26d6
Elias Stagg <38533721+Staggen@users.noreply.github.com>
2019-02-04 19:21:28 +0100

    Revert "Forum Integration"
    This reverts commit ba761789c481350caaeb73e1f51c7efb41b2b45c.

--- ORUComSys/ORUComSys/Controllers/ForumController.cs
+++ ORUComSys/ORUComSys/Controllers/ForumController.cs
@@ -1,21 +1,13 @@
-﻿using Datalayer.Models;
-using Datalayer.Repositories;
-using Microsoft.AspNet.Identity;
-using ORUComSys.Models;
-using System.Collections.Generic;
+﻿using Datalayer.Repositories;
 using System.Web.Mvc;
-using System.Linq;
 
 namespace ORUComSys.Controllers {
-    [Authorize]
     public class ForumController : Controller {
         private PostRepository postRepository;
-        private ProfileRepository profileRepository;
 
         public ForumController() {
             ApplicationDbContext context = new ApplicationDbContext();
             postRepository = new PostRepository(context);
-            profileRepository = new ProfileRepository(context);
         }
 
         public ActionResult Index() { // Select which forum you wish to enter
@@ -23,28 +15,11 @@ namespace ORUComSys.Controllers {
         }
 
         public ActionResult Formal() { // Formal forum
-            return View(ConvertPostsToViewModels(ForumType.Formal));
+            return View();
         }
 
         public ActionResult Informal() { // Informal forum
-            return View(ConvertPostsToViewModels(ForumType.Informal));
-        }
-
-        public PartialViewResult UpdatePosts(string Id) {
-            // Check if the string argument is formal. If it is, set type to Formal, else set it to Informal.
-            ForumType type = string.Equals(Id, "Formal", System.StringComparison.OrdinalIgnoreCase) ? ForumType.Formal : ForumType.Informal; 
-            return PartialView("_ForumPosts", ConvertPostsToViewModels(type));
-        }
-
-        public PostViewModels ConvertPostsToViewModels(ForumType type) {
-            var allPosts = postRepository.GetAllPostsByForumType(type);
-
-            var postViewModel = new PostViewModels {
-                CurrentUser = profileRepository.Get(User.Identity.GetUserId()),
-                PostList = allPosts
-            };
-
-            return postViewModel;
+            return View();
         }
     }
 }
